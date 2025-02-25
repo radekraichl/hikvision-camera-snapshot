1854609cf8f742dc53279dc72b9fd9ff80720a4d
Radek Raichl <50425189+radekraichl@users.noreply.github.com>
2025-02-25 21:46:50 +0100

    Update camera.php

--- camera.php
+++ camera.php
@@ -4,8 +4,8 @@ $password='password';
 $URL='http://0.0.0.0:8000/ISAPI/Streaming/channels/301/picture';	// 301 - camera 3, stream 1
 
 $ch = curl_init();
-//curl_setopt($ch, CURLOPT_TIMEOUT, 30); //timeout after 30 seconds
-//curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
+curl_setopt($ch, CURLOPT_TIMEOUT, 30); //timeout after 30 seconds
+curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
 curl_setopt($ch, CURLOPT_URL,$URL);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
 curl_setopt($ch, CURLOPT_USERPWD, "$username:$password");
@@ -16,7 +16,6 @@ curl_close ($ch);
 if ($status_code == 200)
 {
 	header('Content-Type: image/jpeg');
-
 	$img = imagecreatefromstring($result);
 	imagejpeg($img);
 	imagedestroy($img);
